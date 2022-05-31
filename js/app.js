d50d43a7c3ceae39cfd03bc378e28a71faec6f9f
Jonatan Palavecino <jnpr1992@hotmail.com>
2022-05-30 23:43:44 -0300

    En master: Pude arreglar la carga de articulos. Ahora no puedo lograr que se vea la foto

--- js/app.js
+++ js/app.js
@@ -46,8 +46,6 @@ const modal = document.getElementById('modal')
 
 const vaciarCarrito = document.getElementById('vaciar-carrito')
 
-const cargarProducto = document.getElementById('enviar-producto')
-
 // // ===== DECLARACION DE EVENTOS PARA ABRIR Y CERRAR MODAL =====
 
 botonAbrir.addEventListener('click', () => {
@@ -76,5 +74,3 @@ vaciarCarrito.addEventListener('click', () => {
     mostrarCompra()
     mostrarTotal()
 })
-
-cargarProducto.addEventListener("submit", cargarProductos)
